3a339a92c2501bd5ba72655a795ac4341e4335bc
Huỳnh Đăng Khôi <khoiplbl@gmail.com>
2025-10-25 22:03:15 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import Navbar from "@/components/ui/Navbar";
-import Footer from "@/components/ui/Footer";
+
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -14,11 +13,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="vi">
       <body className="flex flex-col min-h-screen bg-zinc-950 text-white font-sans ">
-        <Navbar />
         <div className={"flex-1"}>
           {children}
         </div>
-        <Footer />
       </body>
     </html>
   );
--- src/components/ui/Footer.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() { 
-    return (
-        <footer className="text-center py-6 text-gray-500 text-sm border-t border-zinc-800">
-          © 2025 ChillFlix Clone – Giao diện học tập
-        </footer>
-    );
-}
