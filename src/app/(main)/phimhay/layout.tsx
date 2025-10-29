5649fade465c4dd371bc332aa10c3968108b85a7
Le Truong Ky <kykhung123123@gmail.com>
2025-10-29 22:52:15 +0700

    update

--- src/app/(main)/phimhay/layout.tsx
+++ src/app/(main)/phimhay/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "../../globals.css";
-import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
+// import Header from "@/components/layout/header";
+// import Footer from "@/components/layout/footer";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
@@ -21,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Header />
       {children}
-      <Footer />
     </>
   );
 }
--- src/services/filmService.ts
+++ src/services/filmService.ts
@@ -1,7 +1,6 @@
 import axios from "@/lib/axios"
 
 const baseURL = process.env.NEXT_PUBLIC_API_BACKEND_URL
-
 export const filmService = {
     async getAll() {
         return axios.get(`${baseURL}/films?current=1&pageSize=10`)
