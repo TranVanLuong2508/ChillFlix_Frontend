486b2f3678d3b2bf98178177b8dc4ccafd4a4a4f
TranVanLuong <tranvanluong032020@gmail.com>
2025-11-12 22:49:48 +0700

    fetch user data from state

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,31 +1,36 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import PlaylistSidebar from "@/components/users/sidebar"
-import ProfileRight from "@/components/users/profile-right"
+import { useEffect, useState } from "react";
+import PlaylistSidebar from "@/components/users/sidebar";
+import ProfileRight from "@/components/users/profile-right";
+import { useAuthStore } from "@/stores/authStore";
 
 export default function ProfilePage() {
-  const [userData, setUserData] = useState({ fullName: "User", email: "user@example.com" })
+  const { authUser, isAuthenticated } = useAuthStore();
+  // const [userData, setUserData] = useState({
+  //   fullName: "User",
+  //   email: "user@example.com",
+  // });
+  const [userEmail, setUserEmail] = useState("");
+  const [userFullName, setUserFullName] = useState("");
 
   useEffect(() => {
-    const authUser = localStorage.getItem("auth-storage")
-    if (authUser) {
-      const user = JSON.parse(authUser)
-      console.log(user.state.authUser)
-
-      setUserData({
-        fullName: user.state.authUser.fullName || "User",
-        email: user.state.authUser.email || "user@example.com",
-      })
+    // const authUser = localStorage.getItem("auth-storage");
+    // if (authUser) {
+    //   const user = JSON.parse(authUser);
+    //   console.log(user.state.authUser);
+    if (isAuthenticated && authUser) {
+      setUserEmail(authUser.email);
+      setUserFullName(authUser.fullName);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="min-h-screen p-8" style={{ backgroundColor: "#1a1d24" }}>
       <div className="flex gap-12 max-w-7xl mx-auto">
-        <PlaylistSidebar userName={userData.fullName} userEmail={userData.email} />
+        <PlaylistSidebar userName={userFullName} userEmail={userEmail} />
         <ProfileRight />
       </div>
     </div>
-  )
+  );
 }
